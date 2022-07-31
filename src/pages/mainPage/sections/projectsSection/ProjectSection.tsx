e31eae66775d9dbbe75580b0fb3ae8a59c9b4d1b
Sean Joseph <69924413+sejogit28@users.noreply.github.com>
2022-07-31 11:21:46 -0500

    MuiPortfolio: Makes updates to styling and order of project card section

--- src/pages/mainPage/sections/projectsSection/ProjectSection.tsx
+++ src/pages/mainPage/sections/projectsSection/ProjectSection.tsx
@@ -1,106 +1,96 @@
-import 
-{ 
-    Card, Grid, CardActions, CardMedia,CardContent, Typography, Tooltip, IconButton, Zoom
-} 
-from '@mui/material';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import LinkIcon from '@mui/icons-material/Link';
+import {
+  Card,
+  Grid,
+  CardActions,
+  CardMedia,
+  CardContent,
+  Typography,
+  Tooltip,
+  IconButton,
+  Zoom,
+} from "@mui/material";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkIcon from "@mui/icons-material/Link";
 
-import projectCardData, {projCardProps} from './projectCardData';
-import StyledGrid from '../../components/StyledGrid';
+import projectCardData, { projCardProps } from "./projectCardData";
+import StyledGrid from "../../components/StyledGrid";
 
-interface ProjectSectionProps {
-    
-}
+interface ProjectSectionProps {}
 
 const ProjectSection = (props: ProjectSectionProps) => {
-    return (
-        <StyledGrid 
-            container 
-            direction={'column'} 
-            justifyContent={"center"} 
-            alignItems={'center'} 
-        >
+  return (
+    <StyledGrid
+      container
+      direction={"column"}
+      justifyContent={"center"}
+      alignItems={"center"}
+    >
+      <Grid item xs={12}>
+        <Typography variant="h3" align="center" gutterBottom>
+          Projects I've Built
+        </Typography>
+      </Grid>
 
-            <Grid item xs={12}>
-                <Typography variant='h3' align='center' gutterBottom>
-                    Projects I've Built
+      {projectCardData.map((projCard: projCardProps) => {
+        return (
+          <Grid
+            item
+            xs={10}
+            md={10}
+            alignSelf="center"
+            sx={{ marginBottom: "6rem" }}
+          >
+            <Card key={projCard.id} sx={{ maxWidth: "50rem" }}>
+              <CardMedia
+                component="img"
+                src={projCard.imageSrc}
+                height="85%"
+                title={projCard.title}
+                sx={{ maxWidth: "100%" }}
+              />
+              <CardContent>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  {projCard.description}
                 </Typography>
-            </Grid>
+              </CardContent>
+              <CardActions disableSpacing>
+                {projCard.liveLink ? (
+                  <Tooltip
+                    arrow
+                    title={projCard.title}
+                    TransitionComponent={Zoom}
+                  >
+                    <IconButton href={projCard.liveLink} aria-label="share">
+                      <LinkIcon fontSize="large" />
+                    </IconButton>
+                  </Tooltip>
+                ) : (
+                  <Tooltip
+                    arrow
+                    title="Live link coming soon"
+                    TransitionComponent={Zoom}
+                  >
+                    <IconButton href="#" aria-label="share">
+                      <LinkIcon fontSize="large" />
+                    </IconButton>
+                  </Tooltip>
+                )}
+                <Tooltip
+                  arrow
+                  title="Project GitHub"
+                  TransitionComponent={Zoom}
+                >
+                  <IconButton href={projCard.gitHubLink} aria-label="share">
+                    <GitHubIcon fontSize="large" />
+                  </IconButton>
+                </Tooltip>
+              </CardActions>
+            </Card>
+          </Grid>
+        );
+      })}
+    </StyledGrid>
+  );
+};
 
-            { projectCardData.map((projCard: projCardProps) =>
-            {
-                return (
-
-                    <Grid item xs={10} md={10} alignSelf='center' sx={{ marginBottom: '6rem' }}>
-                        <Card key={projCard.id} >               
-                            <CardMedia
-                                component="img"
-                                src={projCard.imageSrc}
-                                height="75%"                            
-                                title={projCard.title}
-                                sx={{ maxWidth: '45rem' }}
-                            />
-                            <CardContent>
-                                <Typography variant="body2" color="textSecondary" component="p">
-                                    {projCard.description}
-                                </Typography>
-                            </CardContent>
-                            <CardActions disableSpacing>
-                            {
-                                projCard.liveLink ?
-
-                                <Tooltip       
-                                    arrow 
-                                    title={projCard.title}
-                                    TransitionComponent={Zoom}
-                                >
-                                    <IconButton 
-                                        href={projCard.liveLink}
-                                        aria-label="share"
-                                    >
-                                        <LinkIcon fontSize="large" />
-                                    </IconButton>
-                                </Tooltip>
-
-
-                                :
-
-                                <Tooltip         
-                                    arrow 
-                                    title='Live link coming soon' 
-                                    TransitionComponent={Zoom}
-                                >
-                                    <IconButton 
-                                        href='#'
-                                        aria-label="share"
-                                    >
-                                        <LinkIcon fontSize="large" />
-                                    </IconButton>
-                                </Tooltip>
-                            }
-                                <Tooltip 
-                                    arrow 
-                                    title='Project GitHub' 
-                                    TransitionComponent={Zoom}
-                                >
-                                    <IconButton 
-                                        href={projCard.gitHubLink}
-                                        aria-label="share"
-                                    >
-                                        <GitHubIcon fontSize="large" />
-                                    </IconButton>
-                                </Tooltip>
-                        
-                            </CardActions>
-                        </Card>
-                    </Grid>
-                )
-
-            })}
-
-        </StyledGrid>
-    )
-}
-
-export default ProjectSection
+export default ProjectSection;
--- src/pages/mainPage/sections/projectsSection/projectCardData.ts
+++ src/pages/mainPage/sections/projectsSection/projectCardData.ts
@@ -6,33 +6,33 @@ export interface projCardProps {
   liveLink: string;
   gitHubLink: string;
 }
-const projectCardListData = [
+const projectCardListData: projCardProps[] = [
   {
     id: 1,
-    title: "Bug Track",
-    imageSrc: "/Photos/NewBugTrackerGif.gif",
+    title: "Expense Track",
+    imageSrc: "/Photos/ExpenseTrackerGif.gif",
     description:
-      "A bug tracker built using .NET 5(MVC), Entity Framework, Bootstrap and Microsoft SQL Server",
-    liveLink: "https://the-bug-track-dev.herokuapp.com/",
-    gitHubLink: "https://github.com/sejogit28/BugTrackerTime2Sol",
+      "An expense tracker built with Blazor Web Assembly, .NET 5 Web Api, and Postgres. Deployed on Netlify and Heroku(recently migrated from Microsoft SQL Server/Azure)",
+    liveLink: "https://loving-turing-d3cc22.netlify.app",
+    gitHubLink: "https://github.com/sejogit28/ExpenseTrackerSol",
   },
   {
     id: 2,
     title: "Coding Mind",
     imageSrc: "/Photos/MernBlogGif.gif",
     description:
-      "A personal blog application built using the MERN(MongoDb, Express, React and Node).",
+      "A personal blog application built using the MERN(MongoDb, Express, React and Node). Delployed on Netlify and Heroku",
     liveLink: "https://tender-bohr-ca754b.netlify.app",
     gitHubLink: "https://github.com/sejogit28/MERN-Blog",
   },
   {
     id: 3,
-    title: "Expense Track",
-    imageSrc: "/Photos/ExpenseTrackerGif.gif",
+    title: "Bug Track",
+    imageSrc: "/Photos/NewBugTrackerGif.gif",
     description:
-      "An expense tracker built with Blazor Web Assembly, .NET 5 Web Api, and Microsoft SQL Server",
-    liveLink: "https://loving-turing-d3cc22.netlify.app",
-    gitHubLink: "https://github.com/sejogit28/ExpenseTrackerSol",
+      "A bug tracker built using .NET 5(MVC), Entity Framework, Bootstrap and Postgres. Deployed on Heroku(recently migrated from Microsoft SQL Server/Azure)",
+    liveLink: "https://the-bug-track-dev.herokuapp.com/",
+    gitHubLink: "https://github.com/sejogit28/BugTrackerTime2Sol",
   },
 ];
 
