bdd5eb3a9663129def22fa95bda8464086d335d0
Sean Joseph <69924413+sejogit28@users.noreply.github.com>
2022-07-31 10:57:59 -0500

    MuiPortfolio: Re-orders cards to align with recent changes

--- src/pages/mainPage/sections/projectsSection/projectCardData.ts
+++ src/pages/mainPage/sections/projectsSection/projectCardData.ts
@@ -18,21 +18,21 @@ const projectCardListData = [
   },
   {
     id: 2,
-    title: "Expense Track",
-    imageSrc: "/Photos/ExpenseTrackerGif.gif",
-    description:
-      "An expense tracker built with Blazor Web Assembly, .NET 5 Web Api, and Microsoft SQL Server",
-    liveLink: "https://loving-turing-d3cc22.netlify.app",
-    gitHubLink: "https://github.com/sejogit28/ExpenseTrackerSol",
-  },
-  {
-    id: 3,
     title: "Coding Mind",
     imageSrc: "/Photos/MernBlogGif.gif",
     description:
       "A personal blog application built using the MERN(MongoDb, Express, React and Node).",
     liveLink: "https://tender-bohr-ca754b.netlify.app",
     gitHubLink: "https://github.com/sejogit28/MERN-Blog",
+  },
+  {
+    id: 3,
+    title: "Expense Track",
+    imageSrc: "/Photos/ExpenseTrackerGif.gif",
+    description:
+      "An expense tracker built with Blazor Web Assembly, .NET 5 Web Api, and Microsoft SQL Server",
+    liveLink: "https://loving-turing-d3cc22.netlify.app",
+    gitHubLink: "https://github.com/sejogit28/ExpenseTrackerSol",
   },
 ];
 
